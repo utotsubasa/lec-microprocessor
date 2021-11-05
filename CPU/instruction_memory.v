d68bc22e9e807f6ebf4c87c1207a639b13dd1a1a
utotsubasa <72137006+utotsubasa@users.noreply.github.com>
2021-11-06 00:34:20 +0900

    single cycle processor

---
+++ CPU/instruction_memory.v
@@ -0,0 +1,8 @@
+module instruction_memory(
+        input wire [31:0] r_addr,
+        output wire [31:0] r_data
+    );
+    reg [31:0] mem [0:30000];
+    initial $readmemh("/home/denjo/デスクトップ/後期実験/マイクロプロセッサ/b3exp-master/benchmarks/tests/ZeroRegister/code.hex", mem);
+    assign r_data = mem[r_addr>>2];
+endmodule
