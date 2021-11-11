149389ad5c2d38019e9885019309610301156d08
utotsubasa <72137006+utotsubasa@users.noreply.github.com>
2021-11-11 13:09:40 +0900

    fix bug

--- CPU/data_memory.v
+++ CPU/data_memory.v
@@ -11,17 +11,23 @@ module data_memory(
     output wire [15:0] r_data_half,
     output wire [31:0] r_data_word
 ); 
-    reg [31:0] mem [0:30000];
+    reg [31:0] mem [0:'h7fff];
+        initial $readmemh("/home/denjo/デスクトップ/後期実験/マイクロプロセッサ/b3exp-master/benchmarks/Coremark/data.hex", mem);
     always @(posedge clk) begin
         if(is_store) begin
             case(alucode)
+            /*
                 `ALU_SB: mem[w_addr>>2][(3-w_addr%4)*8+7 -: 8] <= w_data_byte;
                 `ALU_SH: mem[w_addr>>2][(3-w_addr%4)*8+7 -: 16] <= w_data_half;
+                `ALU_SW: mem[w_addr>>2] <= w_data_word;
+                */
+                 `ALU_SB: mem[w_addr>>2][(w_addr%4)*8+7 -: 8] <= w_data_byte;
+                `ALU_SH: mem[w_addr>>2][(w_addr%4)*8+15 -: 16] <= w_data_half;
                 `ALU_SW: mem[w_addr>>2] <= w_data_word;
             endcase
         end
     end
-    assign r_data_byte = mem[r_addr>>2][(3-r_addr%4)*8+7 -: 8];
-    assign r_data_half = mem[r_addr>>2][(3-r_addr%4)*8+7 -: 16];
+    assign r_data_byte = mem[r_addr>>2][(r_addr%4)*8+7 -: 8];
+    assign r_data_half = mem[r_addr>>2][(r_addr%4)*8+15 -: 16];
     assign r_data_word = mem[r_addr>>2];
 endmodule
