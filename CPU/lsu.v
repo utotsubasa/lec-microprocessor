4cb1a1939c1fb74774e921b5bd2ae67e14b40a93
utotsubasa <72137006+utotsubasa@users.noreply.github.com>
2021-11-11 13:11:37 +0900

    fix bug

--- CPU/lsu.v
+++ CPU/lsu.v
@@ -23,15 +23,22 @@ module lsu(
         input [31:0] r_data_word
     );
         case(alucode)
+        /*
             `ALU_LB: load_data_func = {{24{r_data_byte[7]}},r_data_byte};
             `ALU_LH: load_data_func = {{16{r_data_half[7]}},r_data_half[7:0],r_data_half[15:8]};
             `ALU_LW: load_data_func = {r_data_word[7:0],r_data_word[15:8],r_data_word[23:16],r_data_word[31:24]};
             `ALU_LBU: load_data_func = {24'b0,r_data_byte};
             `ALU_LHU: load_data_func = {16'b0,r_data_half[7:0],r_data_half[15:8]};
+            */
+            `ALU_LB: load_data_func = {{24{r_data_byte[7]}},r_data_byte};
+            `ALU_LH: load_data_func = {{16{r_data_half[15]}},r_data_half};
+            `ALU_LW: load_data_func = r_data_word;
+            `ALU_LBU: load_data_func = {24'b0,r_data_byte};
+            `ALU_LHU: load_data_func = {16'b0,r_data_half};
         endcase
     endfunction
     assign load_data = load_data_func(alucode,r_data_byte,r_data_half,r_data_word);
     assign store_data_byte = w_data[7:0];
-    assign store_data_half = {w_data[7:0],w_data[15:8]};
-    assign store_data_word = {w_data[7:0],w_data[15:8],w_data[23:16],w_data[31:24]};
+    assign store_data_half = w_data[15:0];
+    assign store_data_word = w_data[31:0];
 endmodule
