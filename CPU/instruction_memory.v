5bd6001bb0a85daa53b5355d17e62a526e5fdd0a
utotsubasa <72137006+utotsubasa@users.noreply.github.com>
2021-11-11 13:10:54 +0900

    fix bug

--- CPU/instruction_memory.v
+++ CPU/instruction_memory.v
@@ -3,6 +3,6 @@ module instruction_memory(
         output wire [31:0] r_data
     );
     reg [31:0] mem [0:30000];
-    initial $readmemh("/home/denjo/デスクトップ/後期実験/マイクロプロセッサ/b3exp-master/benchmarks/tests/ZeroRegister/code.hex", mem);
+    initial $readmemh("/home/denjo/デスクトップ/後期実験/マイクロプロセッサ/b3exp-master/benchmarks/Coremark/code.hex", mem);
     assign r_data = mem[r_addr>>2];
 endmodule
